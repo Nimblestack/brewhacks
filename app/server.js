ba99c778fe31f81928147a9c5695adb01cefcf44
Aaron <aarongfranco@gmail.com>
2016-04-16 16:08:36 -0400

    making node app

---
+++ app/server.js
@@ -0,0 +1,50 @@
+// SERVER
+// include all the libraries we need as well as variables
+var http = require('http'),
+    express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    server = http.createServer(app);
+
+
+// configure App headers - TODO: use what's necessary
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
+});
+
+// configure static folders
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/img", express.static(__dirname + "/public/img"));
+app.use("/webfonts", express.static(__dirname + "/public/webfonts"));
+app.use("/fonts", express.static(__dirname + "/public/fonts"));
+app.use("/templates", express.static(__dirname + "/public/templates"));
+
+// set up routing
+app.get('/', function (req, res) {
+    console.log(req.url);
+    if (req.url === "/") {
+        fs.readFile(__dirname + '/public/index.html',
+            function (err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error loading ' + __dirname + 'index.html');
+                }
+                res.writeHead(200);
+                res.end(data);
+            });
+    } else {
+        fs.readFile(__dirname+'/public/' + req.url, function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + req.url);
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
+});
+app.listen(Number(process.env.PORT || 5000));
