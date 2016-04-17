0b88443f1ca60e2128e85123ba477c6381215e58
Aaron <aarongfranco@gmail.com>
2016-04-17 11:10:18 -0400

    adding sugar pop

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
-
-	
     $('.beer-open').click(function (e) {
     	e.preventDefault();
     	var d = $(this).attr('data-target');
     	console.log(d)
     	$(d).modal('show');
-        // $(this).next('div').slideToggle();
-        
-        // $(this).parent().siblings().children().next().slideUp();
-        // return false;
     });
 });
 
